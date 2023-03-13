42a58f706f249b9a6a64f69e72997e810517fc63
Biswa Prakash Mishra <biswaprakash444@gmail.com>
2023-03-13 23:20:23 +0530

    error handling for ParseAndValidateArgs func

--- main.go
+++ main.go
@@ -4,11 +4,15 @@ import (
 	"flag"
 	"game-of-pig/simulate"
 	"game-of-pig/strategyparse"
+	"log"
 )
 
 func main() {
 	flag.Parse()
 	args := flag.Args()
-	s, _ := strategyparse.ParseAndValidateArgs(args)
+	s, err := strategyparse.ParseAndValidateArgs(args)
+	if err != nil {
+		log.Fatal(err)
+	}
 	simulate.Simulate(s)
 }
