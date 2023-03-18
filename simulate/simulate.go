995dbb0ff732db0c91d89d2f8a43699759d9dfad
Biswa Prakash Mishra <biswaprakash444@gmail.com>
2023-03-18 10:39:36 +0530

    added story 3 implementation

--- simulate/simulate.go
+++ simulate/simulate.go
@@ -94,6 +94,38 @@ func simulateFixAndVariableStrategy(p int) {
 
 }
 func simulateBothUsersVariableStrategy() {
+	for p1 := 1; p1 <= 100; p1++ { //player 1 holds
+		player1Wins := 0 // set the number of wins to 0 after a strategy simulation
+		player2Wins := 0
+		for p2 := 1; p2 <= 100; p2++ { // for every player 1 holds player 2 plays 99 strategy
+			if p1 != p2 { // 10 games are played for every strategy
+				for i := 0; i < 10; i++ { //play 10 games for every strategy
+					player1Score := 0
+					player2Score := 0
+					for { // start game
+						if player1Score >= 100 {
+							player1Wins += 1
+							break
+						}
+						if player2Score >= 100 {
+							player2Wins += 1
+							break
+						}
+						player1Score += playGame(p1)
+						player2Score += playGame(p2)
+					}
+
+				}
+			}
+		}
+
+		total := player1Wins + player2Wins
+		winPercentage := float64(player1Wins) / float64(total)
+		lossPercentage := float64(player2Wins) / float64(total)
+		fmt.Printf("Result: Wins, losses staying at k = %d: %d/990 (%.2f%%), %d/990 (%.2f%%)\n",
+			p1, player1Wins, winPercentage*100, player2Wins, lossPercentage*100)
+
+	}
 
 }
 func Simulate(s strategyparse.GameStrategy) {
@@ -105,8 +137,7 @@ func Simulate(s strategyparse.GameStrategy) {
 	if s.Story == 2 {
 		simulateFixAndVariableStrategy(s.Player1)
 
-		//simulate story 3
-	}
+	} //simulate story 3
 	if s.Story == 3 {
 		simulateBothUsersVariableStrategy()
 
